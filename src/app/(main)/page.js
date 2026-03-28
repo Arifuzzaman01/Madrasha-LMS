2960c637a3e886252e20de64fc88dde533f3f50f
Arifuzzaman01 <arifuzzamanar29@gmail.com>
2026-03-28 11:10:48 +0600

    setup client for Madrasha Larning Management System

---
+++ src/app/(main)/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>page 
+      <p className='text-3xl font-bold text-[#065F46]'> lorem20 , my name is bangladesh. I am a correpted country</p>
+      <p className='text-3xl font-bold text-[#064E3B]'> lorem20 , my name is bangladesh. I am a correpted country</p>
+    </div>
+  )
+}
